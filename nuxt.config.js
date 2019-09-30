4e62fd9870efcb4a24c1033ba8d0c39a74e2820a
James Didehvar <james@didehvar.com>
2019-09-30 16:03:02 +0100

    firebase example

---
+++ nuxt.config.js
@@ -0,0 +1,90 @@
+export default {
+  mode: 'universal',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    '@nuxtjs/tailwindcss'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    [
+      'nuxt-fire',
+      {
+        // Required:
+        config: {
+          development: {
+            apiKey: 'AIzaSyChKZtFWMkYRm1OcXmAcXjESNGpw52Cj7w',
+            authDomain: 'apex-zones.firebaseapp.com',
+            databaseURL: 'https://apex-zones.firebaseio.com',
+            projectId: 'apex-zones',
+            storageBucket: '',
+            messagingSenderId: '1050652942269',
+            appId: '1:1050652942269:web:44bad1c9d19437d4bd7cd4'
+          },
+          production: {
+            apiKey: 'AIzaSyChKZtFWMkYRm1OcXmAcXjESNGpw52Cj7w',
+            authDomain: 'apex-zones.firebaseapp.com',
+            databaseURL: 'https://apex-zones.firebaseio.com',
+            projectId: 'apex-zones',
+            storageBucket: '',
+            messagingSenderId: '1050652942269',
+            appId: '1:1050652942269:web:44bad1c9d19437d4bd7cd4'
+          }
+        },
+        // The following options are optional:
+        useOnly: [
+          // 'auth',
+          'firestore'
+          // 'functions',
+          // 'storage',
+          // 'realtimeDb',
+          // 'messaging',
+          // 'performance'
+        ]
+      }
+    ]
+  ],
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  }
+}
