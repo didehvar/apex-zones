3f2073202f339c7fa2fbf66fb8e24bb713899833
James Didehvar <james@didehvar.com>
2019-10-01 13:53:47 +0100

    remove default styling

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,10 @@ export default {
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    htmlAttrs: {
+      class: 'antialiased font-sans'
+    }
   },
   /*
    ** Customize the progress-bar color
